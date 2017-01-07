5c3c6448bec423c34243494570f7cf2bdad43257
daniel <daniel.halling@outlook.com>
2017-01-07 20:04:32 +0100

    updated storage to store to disk at 5 days

--- grapher.py
+++ grapher.py
@@ -8,12 +8,13 @@ from pylab import *
 ioff()
 ## make backup of datafile if large##
 rev = 1
-if int(os.stat("./RAM/data.log").st_size) > 1024000:
+if int(os.stat("./RAM/data.log").st_size) > 102400*5:
 	while os.path.isfile("data.log."+str(rev)): rev += 1
 	os.system("mv ./RAM/data.log ./data.log."+str(rev))
 	os.system("tail -n "+str(60*24)+" data.log."+str(rev)+" > ./RAM/data.log")
 	lines= int(subprocess.check_output(["wc", "-l", "data.log."+str(rev)]).split(" ")[0])
 	os.system("head -n "+str(lines-60*24)+" data.log."+str(rev) +" > tmp.log")
+	os.system("rm data.log."+str(rev))
 	os.system("mv tmp.log data.log."+str(rev))
 
 ######################
