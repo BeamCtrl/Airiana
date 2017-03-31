48865140a79fd44808dea80b78d4edbc953aca47
daniel <daniel.halling@outlook.com>
2017-03-31 18:16:20 +0200

    update buttons

--- airiana-core.py
+++ airiana-core.py
@@ -1034,7 +1034,7 @@ if __name__:# not  "__main__":
 		if device.iter %(3600*2)==0:
 			device.get_filter_status()
 			os.system("./http")
-			os.system("./ip-replace.sh")  # reset ip-addresses on buttons.html
+			os.system("./public/ip-replace.sh")  # reset ip-addresses on buttons.html
 			device.get_forcast()
 		## PRINT TO DISPLAY ##
 		device.print_xchanger()
