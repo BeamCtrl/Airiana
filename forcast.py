f2467df900bc0a07b7391cde298ad1cf8ba21971
daniel <daniel.halling@outlook.com>
2017-06-02 14:39:26 +0200

    fixed forcast with fixed paths

--- forcast.py
+++ forcast.py
@@ -5,13 +5,13 @@ import os, time, sys,datetime
 #print dir(os.stat("forecast.xml"))
 #print os.stat("forecast.xml").st_ctime -time.time()
 try:
-	if os.stat("./RAM/forecast.xml").st_ctime -time.time() < -3600 or os.stat("./RAM/forecast.xml").st_size ==0:
+	if os.stat("/home/pi/airiana/RAM/forecast.xml").st_ctime -time.time() < -3600 or os.stat("./RAM/forecast.xml").st_size ==0:
 		#print "Downloading updated forcast" 
-		os.system("wget -q -O ./RAM/forecast.xml http://www.yr.no/sted/Sverige/V%C3%A4stmanland/V%C3%A4ster%C3%A5s-Barkar%C3%B6~2664448/forecast.xml")
+		os.system("wget -q -O /home/pi/airiana/RAM/forecast.xml http://www.yr.no/sted/Sverige/V%C3%A4stmanland/V%C3%A4ster%C3%A5s-Barkar%C3%B6~2664448/forecast.xml")
 except Exception as err:
 		print "Error getting Forcast from YR.no"#os.system("sudo wget  -q -O forecast.xml http://www.yr.no/sted/Sverige/V%C3%A4stmanland/V%C3%A4ster%C3%A5s-Barkar%C3%B6~2664448/forecast.xml")
 		print err
-		os.system("touch ./RAM/forecast.xml")
+		os.system("touch /home/pi/airiana/RAM/forecast.xml")
 class Weather():
 	def __init__(self):
 		self.wind_speed = 0
@@ -27,7 +27,7 @@ class Weather():
 	def __str__(self):
 	 	 return time.asctime(self.valid_from)+" "+str(self.temp)+"C "+str(self.pressure)+"hPa "+str(self.precipitation)+"mm "+str(self.wind_speed)+"m/s Weather type: "+str(self.weather_type)+" "
 parser = xml.parsers.expat.ParserCreate()
-fd = open('./RAM/forecast.xml','r')
+fd = open('/home/pi/airiana/RAM/forecast.xml','r')
 forcasts = []
 def start_element(name, attrs):
 		#print name , attrs
