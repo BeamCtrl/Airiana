a151c77645c48463a91ef069ab0102fddf44019b
daniel.halling@outlook.com <daniel.halling@outlook.com>
2018-04-03 19:39:31 +0200

    completed external updater

--- updater.py
+++ updater.py
@@ -1,6 +1,13 @@
-os.system("wget -q -O ./RAM/VERS http://lappy.asuscomm.com:443/current_$
+#!/usr/bin/python
+import os, sys
+os.system("wget -q -O ./RAM/VERS http://lappy.asuscomm.com:443/current_version")
 ver = os.popen("cat ./RAM/VERS").read()
 ver = ver.split(" ")
+tmp = os.popen("head airiana-core.py").readlines()
+for each in tmp:
+	if "vers" in each:
+		temp = each.split("=")
+		vers = temp[-1][2:-2]
 
 if "debug" in sys.argv:
 	print vers, "->", ver
