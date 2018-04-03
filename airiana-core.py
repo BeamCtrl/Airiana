a914db9b3b3076e8cfad71774783030ffb105ddb
daniel.halling@outlook.com <daniel.halling@outlook.com>
2018-04-03 20:12:56 +0200

    removed auto update from inside core

--- airiana-core.py
+++ airiana-core.py
@@ -133,18 +133,6 @@ def update_sensors():
 	except IndexError:
 		device.msg += "\nnew sensor data error"
 		traceback.print_exc()
-#Auto updater
-def update():
-	os.system("wget -q -O ./RAM/VERS http://lappy.asuscomm.com:443/current_version")
-	ver = os.popen("cat ./RAM/VERS").read()
-	ver = ver.split(" ")
-	
-	if "debug" in sys.argv:
-		print vers, "->", ver
-	if vers not in ver[0] and "Valid" in ver[1]:
-		print "Updating Airiana system software to",ver[0]
-		os.system("./update&")	
-	
 #WRITE TO DATA.LOG
 def logger ():
 	try:
