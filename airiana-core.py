7622368cab52118df2172972ded7a679d7ae2938
daniel.halling@outlook.com <daniel.halling@outlook.com>
2018-06-17 15:39:06 +0200

    go to 8.3

--- airiana-core.py
+++ airiana-core.py
@@ -4,7 +4,7 @@ import airdata, serial, numpy, select, threading, minimalmodbus
 import os, traceback, time, sys, signal
 #from mail import *
 ############################
-vers = "8.2"
+vers = "8.3"
 Running =True
 savecair=False
 # Register cleanup
