aa3e21fdce2d804ec69dc62c527ed62f6d72da94
daniel.halling@outlook.com <daniel.halling@outlook.com>
2018-11-18 21:54:38 +0100

    vers to 8.7

--- airiana-core.py
+++ airiana-core.py
@@ -4,7 +4,7 @@ import airdata, serial, numpy, select, threading, minimalmodbus
 import os, traceback, time, sys, signal
 #from mail import *
 ############################
-vers = "8.6"
+vers = "8.7"
 Running =True
 savecair=False
 # Register cleanup
