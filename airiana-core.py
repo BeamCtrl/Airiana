4813225e8618db012c2973594ae97021e35349ca
daniel.halling@outlook.com <daniel.halling@outlook.com>
2019-05-17 13:39:32 +0200

    Relase moved to 9.5

--- airiana-core.py
+++ airiana-core.py
@@ -5,7 +5,7 @@ import airdata, serial, numpy, select, threading, minimalmodbus
 import os, traceback, time, sys, signal
 #from mail import *
 ############################
-vers = "9.4"
+vers = "9.5"
 Running =True
 savecair=False
 # Register cleanup
