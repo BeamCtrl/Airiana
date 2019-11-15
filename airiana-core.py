037fc369e3b22a3cf59291c8c10828f486689cff
daniel.halling@outlook.com <daniel.halling@outlook.com>
2019-11-15 20:21:40 +0100

    to 9.u

--- airiana-core.py
+++ airiana-core.py
@@ -6,7 +6,7 @@ import os, traceback, time, sys, signal
 from request import Request
 #from mail import *
 #############################
-vers = "9.t"
+vers = "9.u"
 Running =True
 savecair=False
 # Register cleanup
