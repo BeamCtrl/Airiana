beee76f09f7aeeb33dd8a50d8babd13006277cb2
danielhalling <daniel.halling@outlook.com>
2019-11-15 23:26:22 +0100

    cleaned up Request.py with pyCharm

--- request.py
+++ request.py
@@ -2,121 +2,126 @@
 # -*- coding: utf-8 -*-
 import time
 import os
-import sys
 
-#######Request object for doing modbus comms##
-class Request(object):
-	def __init__(self,bus,client):
-		self.connect_errors= 0
-		self.checksum_errors = 0
-		self.multi_errors = 0
-		self.write_errors = 0
-		self.buff= ""
-		self.counter = 0
-		self.error_time=time.time()
-		self.wait_time =  0.01
-		self.prev_rate = 0
-		self.rate =0
-		self.iter = 0
-		self.bus = bus
-		self.client = client
-	def modbusregisters(self,start,count,signed=False):
-		self.client.precalculate_read_size=True
-		self.iter += 1
-		try:
-			self.response= "no data"
-			self.response = self.client.read_registers(start,count)
-			if signed:
-				for each in self.response:
-					if each & 0x8000: each -= 0xFFFF
-		except ValueError as error:
-			#print "multi, checksum error,retry:",self.checksum_errors,error.message,";"
-			if  error.message.find("\x01\x83\x02\xc0\xf1")<>-1:
-				print "multi, address out of range;"
-				#exit()
-			self.checksum_errors +=1
-			self.modbusregisters(start,count)
-		except IOError as error:
-			self.connect_errors += 1
-			if self.connect_errors > 100 or self.multi_errors >100:
-				self.error_review()
-			if self.rate < 0.9:
-				self.modbusregisters(start,count)
-		self.client.precalculate_read_size=False
+"""#######Request object for doing modbus coms##
+"""
 
-	def error_review (self):
-		delta = self.iter - self.error_time
-		self.error_time = self.iter
-		if delta <>0:
-			rate = float(self.connect_errors+self.checksum_errors+self.write_errors+self.multi_errors) / delta
-		else: rate=0.5
-		if rate >= 0.9:
-			os.read(self.bus,1000)
-			time.sleep(1)
-			fd = os.open("RAM/err",os.O_WRONLY)
-			os.write(fd,"read error high rate, possible no comms with unit error rate over 90%\n")
-			os.close(fd)
-			raise IOError
-		os.system ("echo "+str(rate)+" "+ str(self.wait_time)+" > RAM/error_rate")
-		self.connect_errors = 0
-		self.checksum_errors = 0
-		self.write_errors = 0
-		self.multi_errors = 0
 
-	def modbusregister (self,address,decimals):
-		self.iter += 1
-		self.client.precalculate_read_size=True
+class Request:
+    connect_errors = 0
+    checksum_errors = 0
+    multi_errors = 0
+    write_errors = 0
+    buff = ""
+    counter = 0
+    error_time = time.time()
+    wait_time = 0.01
+    prev_rate = 0
+    rate = 0
+    iter = 0
+    bus = 0
+    client = ""
+    response = ""
 
-		try:
-			self.response = "no data"
-			self.buff += os.read(self.bus,20) # bus purge
-                        self.response = self.client.read_register(address,decimals,signed=True)
-		except IOError as error:
-	 		self.connect_errors += 1
-			if self.connect_errors > 100: self.error_review()
-			self.buff += os.read(self.bus,20) # bus purge
-			#print "single, no response, retry:",self.connect_errors,address,";"
-			self.modbusregister(address,decimals)
-		except ValueError as error:
-			#print "single, checksum error,retry:",self.checksum_errors,error,";"
-			#os.write(ferr,"read: "+str(error)+"\n")
-			self.buff += os.read(self.bus,20) # bus purge
-			self.checksum_errors +=1
-			self.modbusregister(address,decimals)
-		self.client.precalculate_read_size=False
+    def __init__(self, bus, client):
+        self.client = client
+        self.bus = bus
+        print "request object created"
 
-	def write_register(self, reg, value,functioncode=6,tries=10):
-		self.iter += 1
-		self.client.precalculate_read_size=True
-		#if start == value: return 0
-		try:
-			#print "set", reg, "to",value
-			#time.sleep(self.wait_time)
-			#print os.read(bus,20)
-			if tries  > 0:
-				resp = self.client.write_register(reg,value,0,6)
-			#print tries,reg
-			#sys.stdout.flush()
-		except IOError as error:
-			#print "write, ioerror",error,os.read(self.bus,20),";"
-			#os.write(ferr,"write: "+str(error)+"\n")
-			self.write_errors += 1
-			if tries  > 0:
-				self.write_register(reg,value,tries=tries-1)
-			pass
-		except ValueError as error:
-			#print "write, val error",error,os.read(self.bus,20),"\n--",reg," ",value,";"
-			#os.write(ferr,"write: "+str(error)+"\n")
-			self.write_errors += 1
-			if tries  > 0:
-				self.write_register(reg,value,tries=tries-1)
+    def modbusregisters(self, start, count, signed=False):
+        self.client.precalculate_read_size = True
+        self.iter += 1
+        try:
+            self.response = "no data"
+            self.response = self.client.read_registers(start, count)
+            if signed:
+                for each in self.response:
+                    if each & 0x8000:
+                        each -= 0xFFFF
+        except ValueError as error:
+            if -1 != error.message.find("\x01\x83\x02\xc0\xf1"):
+                print "multi, address out of range;"
+            # exit()
+            self.checksum_errors += 1
+            self.modbusregisters(start, count)
+        except IOError:
+            self.connect_errors += 1
+            if self.connect_errors > 100 or self.multi_errors > 100:
+                self.error_review()
+            if self.rate < 0.9:
+                self.modbusregisters(start, count)
+        self.client.precalculate_read_size = False
 
-			pass
-		self.modbusregister(reg,0)
-		if value <> self.response and tries>0:
-			self.write_register(reg,value,tries=tries-1)
-		if tries == 0:
-			fd = os.open("RAM/err",os.O_WRONLY)
-			os.write(fd,"Write error, no tries left on register:"+str(reg)+"\n")
-			os.close(fd)
+    def error_review(self):
+        delta = self.iter - self.error_time
+        self.error_time = self.iter
+        if delta != 0:
+            rate = float(self.connect_errors + self.checksum_errors + self.write_errors + self.multi_errors) / delta
+        else:
+            rate = 0.5
+        if rate >= 0.9:
+            os.read(self.bus, 1000)
+            time.sleep(1)
+            fd = os.open("RAM/err", os.O_WRONLY)
+            os.write(fd, "read error high rate, possible no comms with unit error rate over 90%\n")
+            os.close(fd)
+            raise IOError
+        os.system("echo " + str(rate) + " " + str(self.wait_time) + " > RAM/error_rate")
+        self.connect_errors = 0
+        self.checksum_errors = 0
+        self.write_errors = 0
+        self.multi_errors = 0
 
+    def modbusregister(self, address, decimals):
+        """
+
+        :type self: request Object for modbus comm
+        """
+        self.iter += 1
+        self.client.precalculate_read_size = True
+
+        try:
+            self.response = "no data"
+            self.buff += os.read(self.bus, 20)  # bus purge
+            self.response = self.client.read_register(address, decimals, signed=True)
+        except IOError:
+            self.connect_errors += 1
+            if self.connect_errors > 100:
+                self.error_review()
+            self.buff += os.read(self.bus, 20)  # bus purge
+            self.modbusregister(address, decimals)
+        except ValueError:
+            self.buff += os.read(self.bus, 20)  # bus purge
+            self.checksum_errors += 1
+            self.modbusregister(address, decimals)
+        self.client.precalculate_read_size = False
+
+    def write_register(self, reg, value, tries=10):
+        self.iter += 1
+        self.client.precalculate_read_size = True
+
+        try:
+
+            if tries > 0:
+                self.client.write_register(reg, value, 0, 6)
+
+        except IOError:
+
+            self.write_errors += 1
+            if tries > 0:
+                self.write_register(reg, value, tries=tries - 1)
+            pass
+        except ValueError:
+
+            self.write_errors += 1
+            if tries > 0:
+                self.write_register(reg, value, tries=tries - 1)
+
+            pass
+        self.modbusregister(reg, 0)
+        if value != self.response and tries > 0:
+            self.write_register(reg, value, tries=tries - 1)
+        if tries == 0:
+            fd = os.open("RAM/err", os.O_WRONLY)
+            os.write(fd, "Write error, no tries left on register:" + str(reg) + "\n")
+            os.close(fd)
