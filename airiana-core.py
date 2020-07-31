924e876e42bbaebdda6c2af5a3fbf1cd40da27a7
Daniel Halling <daniel.halling@outlook.com>
2020-07-31 19:01:52 +0200

    added device USB0 as first check for availible chardevice to modbus

--- airiana-core.py
+++ airiana-core.py
@@ -60,7 +60,10 @@ if "daemon" in sys.argv:
 	os.lseek(ferr,0, os.SEEK_END)
 
 # Setup serial, RS 485 to machine
-if os.path.lexists("/dev/serial0"):
+if os.path.lexists("/dev/ttyUSB0"):
+	print "Communication started on device ttyUSB0;"
+	unit = "/dev/serial0"
+elif os.path.lexists("/dev/serial0"):
 	print "Communication started on device Serial0;"
 	unit = "/dev/serial0"
 	#os.system("sleep 2")
