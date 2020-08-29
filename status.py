c3b5c5ea2cce5baaceab5c51ebe7e4fe2b0952a5
Daniel Halling <daniel.halling@outlook.com>
2020-08-29 10:32:05 +0200

    bugfixes to mail-sender andress user dict usage layout and registered user handling

--- status.py
+++ status.py
@@ -25,26 +25,24 @@ files = os.listdir("./public/local_links/")
 data = dict(enumerate(["token","exchanger","exectime","name","version","hash","uptime","inlet","extract","extractflow","rh","auto","cooling","supply","exhaust","pdiff","detlimit"]))
 data = {value : key for (key, value) in data.items()}
 
-print data
+#print data
 #for each in users.keys():
 #	print each, ": ",users[each]
 stat_dict = {}
 location= dict()
 def checkLocation(user):
-	try:	
-		print location
+	try:
+		#print location
 		if len(location[user]) <>0:
-			print "user known",user
+			#print "user known",user, location[user]
 			return None
 	except KeyError:
 		with open("./public/local_links/"+user+".html") as log:
 			ip = log.read().split("Source:('")[-1].split("\'")[0]
-		print "checking ip:",ip
+		#print "checking ip:",ip
 		loc = os.popen("/home/pi/airiana/geoloc.py " + ip).read()
 		location.update( {user:loc})
-		print "got new location", loc
-	
-
+		#print "got new location", loc
 def analyse_stat(status,user):
 	#if "debug" in sys.argv:
 	#	print "Analyze", status, users[user]
@@ -98,6 +96,7 @@ while True:
 			try:
 				mod = os.stat(str("./public/local_links/"+each)).st_mtime
 				content = os.popen("cat ./public/local_links/"+each).readlines()
+				stat_field = ""
 				for line in content:
 					if "status:" in line:
 						stat_field = line
@@ -106,76 +105,87 @@ while True:
 				#print content
 				user =str(each.split(".")[0])
 				checkLocation(user)
-				try: print "userID",user,"location:", location[user]
-				except:print "\n"
+				#try: print "userID",user,"location:", location[user]
+				#except:
+				#	print "\n"
 				try:
 					if content.find("status")!=-1 :
 						exec ("lis ="+stat_field.split(":")[-1])
 						#print lis
-					else: lis =[]
+					else: lis =["no data"]
 				except IOError:
 					lis=["data error"]
-				#print lis
-				if int(time.time()-starttime)%1 == 0:
+				# CHECK IF USER REGISTERED #
+				if user in users.keys():
+					#print "Checking status of user ", users[user],"\n"
+					if int(time.time()-starttime)%1 == 0:
+						try:
+							analyse_stat(lis,user)
+						except IOError: print "analysis error"
+					#print status[str(each.split(".")[0])], each
+					if (time.time()-mod)/3600>status[user]:
+						status[user] = round((time.time()-mod)/3600,2)
+					status[user] =status[user]*0.99
+					if (time.time()-mod)/3600<3:
+						flag = "<font color=\"green\"> Alive </font>"
+						if  mail_sent[user] == True:
+	                                                mail_sent[user] = False
+	                                                mailer.setup ("daniel.halling@outlook.com","airiana@outlook.com","Airiana user: "+str(users[user])+" has checked in and is now alive.")
+	                                                mailer.send()
+					else:
+						flag = "<font color=\"red\"> Inactive </font>"
+						if not mail_sent[user]:
+							mail_sent[user] = True
+							mailer.setup ("daniel.halling@outlook.com","airiana@outlook.com","Airiana user: "+str(users[user])+" has changed status to inactive.")
+							mailer.send()
+					status_table= ""
+					for item in lis:
+						status_table += str(item) +"</td><td>"
+					if (time.time()-mod)/3600<3:
+						html += "<tr><td><a href=\"/local_links/"\
+							+each+"\">"+users[user]\
+							+"</a></td><td>"+time.strftime("%d/%m %H:%M:%S",time.localtime(mod))+"</td><td>"+flag\
+							+"</td><td> "\
+							+status_table
+					else:
+						html += "<tr style=\"font-style:italic\" ><td><a href=\"/local_links/"\
+							+each+"\">"+users[user]\
+							+"</a></td><td>"+time.strftime("%d/%m %H:%M:%S",time.localtime(mod))+"</td><td>"+flag\
+							+"</td><td> "\
+							+status_table
+
 					try:
-						analyse_stat(lis,user)
-					except IOError: print "analysis error"
-				#print status[str(each.split(".")[0])], each
-				if (time.time()-mod)/3600>status[str(each.split(".")[0])]:
-					status[str(each.split(".")[0])] = round((time.time()-mod)/3600,2)
-				status[str(each.split(".")[0])] =status[str(each.split(".")[0])]*0.99
-				if (time.time()-mod)/3600<3:
-					flag = "<font color=\"green\"> Alive </font>"
-					if  mail_sent[each.split(".")[0]] == True:
-                                                mail_sent[each.split(".")[0]] = False
-                                                mailer.setup ("daniel.halling@outlook.com","airiana@abiding.se","Airiana user: "+str(users[str(each.split(".")[0])])+" has checked in and is now alive.")
-                                                mailer.send()
+						html += " " +str(stat_dict[user])
+					except KeyError: pass #print "No error state"
+					try:
+						html += " </td><td>"+location[user]+"</td></tr>\n"
+					except:
+						html += "</td></tr>\n"
+
 				else:
-					flag = "<font color=\"red\"> Inactive </font>"
-					if not mail_sent[each.split(".")[0]]:
-						mail_sent[each.split(".")[0]] = True
-						mailer.setup ("daniel.halling@outlook.com","airiana@outlook.com","Airiana user: "+str(users[str(each.split(".")[0])])+" has changed status to inactive.")
-						mailer.send()
-				status_table= ""
-				for item in lis:
-					status_table += str(item) +"</td><td>"
-				if (time.time()-mod)/3600<3:
-					html += "<tr><td><a href=\"/local_links/"\
-						+each+"\">"+users[str(each.split(".")[0])]\
+					if (time.time()-mod)/3600<3:
+						flag = "<font color=\"green\"> Alive </font>"
+					else:
+						flag = "<font color=\"red\"> Inactive </font>"
+
+					status_table= ""
+					for item in lis:
+						status_table += str(item) +"</td><td>"
+					html2 += "<tr><td><a href=\"/local_links/"\
+						+each+"\">"+user\
 						+"</a></td><td>"+time.strftime("%d/%m %H:%M:%S",time.localtime(mod))+"</td><td>"+flag\
 						+"</td><td> "\
 						+status_table
-				else:
-					html += "<tr style=\"font-style:italic\" ><td><a href=\"/local_links/"\
-						+each+"\">"+users[str(each.split(".")[0])]\
-						+"</a></td><td>"+time.strftime("%d/%m %H:%M:%S",time.localtime(mod))+"</td><td>"+flag\
-						+"</td><td> "\
-						+status_table
+					try:
+						html2 += " </td><td>"+location[user]+"</td></tr>\n"
+					except:
+						html2 += "</td></tr>\n"
 
-				try:
-					html += " " +str(stat_dict[user])
-				except KeyError: pass
-				try:
-					html += " </td><td>"+location[user]+"</td></tr>\n"
-				except:
-					html += "</td></tr>\n"
-
+					if (time.time()-mod)/3600 > 24:
+						os.system("rm -f ./public/local_links/"+each)
 			except KeyError:
-				status_table= ""
-				for item in lis:
-					status_table += str(item) +"</td><td>"
-				html2 += "<tr><td><a href=\"/local_links/"\
-					+each+"\">"+str(each.split(".")[0])\
-					+"</a></td><td>"+time.strftime("%d/%m %H:%M:%S",time.localtime(mod))+"</td><td>"+flag\
-					+"</td><td> "\
-					+status_table
-				try:
-					html2 += " </td><td>"+location[user]+"</td></tr>\n"
-				except:
-					html2 += "</td></tr>\n"
+				pass
 
-				if (time.time()-mod)/3600 > 24:
-					os.system("rm -f ./public/local_links/"+each)
 		html +="<br></table><br>"
 		html += """	<table style="width: 100%">
 			<caption style="text-align:left"><strong>Not registered users:</strong></caption>
