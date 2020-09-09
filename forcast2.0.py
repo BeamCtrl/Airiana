eb2dcb3e483a58951c71e31d31f6f9f7e064e9de
Daniel Halling <daniel.halling@outlook.com>
2020-09-09 14:17:35 +0200

    missing / on user agent

--- forcast2.0.py
+++ forcast2.0.py
@@ -64,7 +64,7 @@ if not os.path.lexists("RAM/forecast.json"): os.system("touch RAM/forecast.json"
 if os.stat("RAM/forecast.json").st_ctime - time.time() < -3600 or os.stat("RAM/forecast.json").st_size == 0 or "-f" in sys.argv:
 	#print "updateing forcast", os.stat("RAM/forecast.json").st_ctime - time.time() 
 	loc = "\"https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=" + str(latlong["lat"]) + "&lon="+str(latlong["long"])+"\""
-	os.system("wget -q -U \"Airiana-forecast-agent github.com/beamctrl/Airiana/" -O /home/pi/airiana/RAM/forecast.json "+loc)
+	os.system("wget -q -U \"Airiana-forecast-agent github.com/beamctrl/Airiana/\" -O /home/pi/airiana/RAM/forecast.json "+loc)
 #get long lat alt from forecast
 with open("RAM/forecast.json") as source:
 	data = json.load(source)
