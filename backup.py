6ba4ea74ea95dfd8a916ec94e8f35c0488fc2806
Daniel Halling <daniel.halling@outlook.com>
2022-01-29 10:59:20 +0100

    update backup.py to python3

--- backup.py
+++ backup.py
@@ -1,22 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import time as tm
 import os
 ## make backup of datafile if large##
-logfile = open("./RAM/data.log","r")
+logfile = ""
+try:
+	logfile = open("./RAM/data.log","r")
+except FileNotFoundError:
+	print("no datafile found, creating")
+	os.system("touch ./RAM/data.log")
+	exit(0)
 loglines = logfile.readlines()
 buffer = []
 buffer_len = -1
-while buffer_len <> len(buffer) :
+while buffer_len != len(buffer) :
 	buffer_len = len(buffer)
 	for each in loglines:
 		line = each.split(":")
 		if float(line[0]) < (tm.time()-(25*60*60)):
-			os.system("echo -n \""+each +"\">> ./data.log")
+			os.system("echo -n \"" + each + "\">> ./data.log")
 			buffer.append (loglines.pop(loglines.index(each)))
-			#print "removed:",tm.ctime(float(line[0]))
 
 os.system("rm -f ./RAM/data.log")
-targetfile = open("./RAM/data.log","w+")
+targetfile = open("./RAM/data.log", "w")
 for each in loglines:
 	targetfile.write(each)
 targetfile.close()
