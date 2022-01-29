9605b383afc679c06fe7d8f7739afbfac6c666d0
Daniel Halling <daniel.halling@outlook.com>
2022-01-29 22:13:13 +0100

    python3, fixed bytes obj on pickle load

--- airiana-core.py
+++ airiana-core.py
@@ -468,7 +468,7 @@ class Systemair(object):
     def system_setup(self):
         try:
             if os.path.isfile("coeficients.dat"):
-                self.coef_dict = pickle.load(open("coeficients.dat"))
+                self.coef_dict = pickle.load(bytes(open("coeficients.dat")))
             else:
                 raise IOError
         except:
