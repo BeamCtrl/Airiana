1ed4918e76a3638199a1fccadb56b7e91fbc83c0
Daniel Halling <daniel.halling@outlook.com>
2022-02-19 23:10:30 +0100

    update upgrade.sh for automatic distr upgrade

--- systemfiles/upgrade.sh
+++ systemfiles/upgrade.sh
@@ -1,5 +1,8 @@
+sed "s/$1/$2/g" /etc/apt/sources.list > /etc/apt/sources.list
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
 apt-get -qy update
 apt-get -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confnew" upgrade
+apt-get -qy autoremove
 apt-get -qy autoclean
+apt-get -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confnew" dist-upgrade
