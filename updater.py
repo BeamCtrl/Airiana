9cea00b423dfd96d05eccf9e45b95a7da0e872b1
Daniel Halling <daniel.halling@outlook.com>
2022-02-21 17:03:12 +0100

    python2.7 import of updater.py

--- updater.py
+++ updater.py
@@ -1,14 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 import os, sys
-os.chdir("/home/pi/airiana/")
+#os.chdir("/home/pi/airiana/")
 deb_versions = ("wheezy", "jessie", "stretch", "buster", "bullseye",
-                "bookworm", "trixie")
+                "bookworm", "trixie", "focal")
 
+os_name = os.popen("./osname.py").readline()[:-1]
 for system in enumerate(deb_versions):
-    os_name = os.popen("./osname.py").readline()[:-1]
-    if os_name in system:
+    if os_name in system and deb_versions.index(os_name) < len(deb_versions)-1:
         print("current", system[1])
         print("future", deb_versions[system[0]+1])
+        print("updating to future")
+        os.system("./systemfiles/upgrade.sh " + system[1] + " " + deb_versions[system[0]+1])
+    elif os_name == deb_versions[-1]:
+        print("os codename checked [" + os_name + "], we're good.")
+        break
 
 os.system("git fetch && git checkout -m origin/master ./public/current_version" )
 ver = os.popen("cat ./public/current_version").read()
