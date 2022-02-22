b1aaf52b921be7cb25a5643d0a6f7465fd9ea57c
Daniel Halling <daniel.halling@outlook.com>
2022-02-22 23:15:06 +0100

    tee sudo to allow update of .list

--- systemfiles/upgrade.sh
+++ systemfiles/upgrade.sh
@@ -19,9 +19,9 @@ sudo -E apt-get -q autoclean
 
 #update the distro sources
 apt=$(sed "s/$1/$2/g" /etc/apt/sources.list)
-sudo echo $apt |tee  /etc/apt/sources.list
+sudo echo $apt |sudo tee  /etc/apt/sources.list
 apt2=$(sed "s/$1/$2/g" /etc/apt/sources.list.d/raspi.list)
-sudo echo $apt2 |tee  /etc/apt/sources.list.d/raspi.list
+sudo echo $apt2 |sudo tee  /etc/apt/sources.list.d/raspi.list
 
 # update/upgrade/dist-upgrade
 sudo -E apt-get -q update
