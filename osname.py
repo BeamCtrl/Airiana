c8201dcbc599f7dce26f522010f16cd9b970967d
Daniel Halling <daniel.halling@outlook.com>
2022-02-22 22:47:31 +0100

    it starts at jessie

--- osname.py
+++ osname.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import os
-
+osname = "none"
 tmp = os.popen("cat /etc/os-release")
 for line in tmp.readlines():
     if "VERSION_CODENAME" in line:
         osname = line.split("=")[-1][:-1]
-print (osname)
+if osname != "none":
+    print(osname)
+else:
+    print("jessie")
