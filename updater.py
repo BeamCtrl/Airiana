fd7b968483e0706084866dc927fe175012fcd72a
Daniel Halling <daniel.halling@outlook.com>
2022-02-22 22:55:26 +0100

    missing space

--- updater.py
+++ updater.py
@@ -13,7 +13,7 @@ for system in enumerate(deb_versions):
         print("current", system[1])
         print("future", deb_versions[system[0]+1])
         print("Updating to", deb_versions[system[0]+1])
-        print("./systemfiles/upgrade.sh " + system[1] + deb_versions[system[0] + 1])
+        print("./systemfiles/upgrade.sh " + system[1] + " " + deb_versions[system[0] + 1])
         os.system("./systemfiles/upgrade.sh " + system[1] + " " + deb_versions[system[0]+1])
         os.system("sudo reboot")
 os.chdir(path)
