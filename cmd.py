68288023a72dd2e6591b19ae01edf4d92c41a9f5
Daniel Halling <daniel.halling@outlook.com>
2022-03-13 18:30:37 +0100

    fix shadowed input variable

--- cmd.py
+++ cmd.py
@@ -19,18 +19,18 @@ print(chr(27)+"[2J")
 #cmds()
 try:
 	while True:
-		input= select.select([sys.stdin],[],[],2)
-		if sys.stdin in input[0]:
+		incomming = select.select([sys.stdin],[],[],2)
+		if sys.stdin in incomming[0]:
 			cmd = sys.stdin.read(2)
 			print(cmd+chr(27)+"[2J")
 			sock.sendto(cmd.encode("utf-8"),("127.0.0.1",9876))
 		print(chr(27) +"\x1b[H")
-		#chr(27) +"[2J\x1b[H"
+		# chr(27) +"[2J\x1b[H"
 		page = open("./RAM/out").read()
 		if page.find("break")>=0:
 			os.system("clear")
 			print(page)
-			raw_input("press enter to resume") 
+			input("press enter to resume")
 			print(chr(27) +"[2J\x1b[H")
 		else:
 			os.system("clear")
