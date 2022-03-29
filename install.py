3f89f3f5744bb2ec98fd790745b957a589248352
Daniel Halling <daniel.halling@outlook.com>
2022-03-29 20:36:36 +0200

    refactor and mod sudo rights into install

--- install.py
+++ install.py
@@ -16,7 +16,87 @@ import os
 import sys
 import pwd
 
-user_id = pwd.pwuid()
+def setUart():
+    global boot_cmd, lines, reboot
+    # Enable UART for RS485 HAT
+    boot_cmd = "enable_uart=1\n"
+    boot_file = open("/boot/config.txt", "r+")
+    lines = boot_file.readlines()
+    if boot_cmd in lines:
+        print("Uart already enabled")
+    else:
+        print("Enabling uart")
+        boot_file.write(boot_cmd)
+        boot_file.close()
+        reboot = True
+    sys.stdout.flush()
+    exit(0)
+
+
+# INSTALL DEPS #
+def install_deps():
+    os.system("apt-get -y install python3-pip")
+    os.system("apt-get -y install python3-dev")
+    os.system("apt-get -y install python3-matplotlib")
+    os.system("pip3 install minimalmodbus")
+    os.system("pip3 install progressbar")
+    os.system("pip3 install requests")
+    os.system("pip3 install pyModbusTCP")
+    os.system("pip3 install setuptools")
+    os.system("pip3 install pyephem")
+    os.system("pip3 install numpy")
+    os.system("apt-get -y install ntp")
+    os.system("apt-get -yq --force-yes -o \"Dpkg::Options::=--force-confdef\"  upgrade")
+
+
+def setFstab():
+    global lines, reboot, user_id, group_id
+    # MAKE RAM DRIVE IN FSTAB#
+    fstab_comment = "#temp filesystem only in RAM for use on Airiana tempfiles.\n"
+    fstab_RAM = "tmpfs " + path + "/RAM tmpfs defaults, noatime, nosuid, uid=" + user_id + ", gid=" + group_id + ", mode=0755, size=50m 0 0\n"
+    # MAKE RAM DRIve for linux logs var/logs
+    fstab_var = "tmpfs /var/log tmpfs defaults, noatime, nosuid, mode=0755, size=75m 0 0"
+    fstab_file = open("/etc/fstab", "r+")
+    lines = fstab_file.readlines()
+    # Write RAM tmpfs's to fstab
+    if fstab_var not in lines or fstab_RAM not in lines:
+        for each in lines:
+            if "/RAM" in each:
+                print("Found RAM drive setup")
+                lines.pop(lines.index(each))
+            if "/var" in each:
+                print("Found /var/log partition setup")
+                lines.pop(lines.index(each))
+            if "filesystem only in RAM" in each:
+                print("Found fstab comment")
+                lines.pop(lines.index(each))
+        print("Setting up Ram drive")
+        lines.append(fstab_comment)
+        lines.append(fstab_RAM)
+        lines.append(fstab_var)
+        fstab_file.seek(0, 0)
+        fstab_file.writelines(lines)
+        fstab_file.close()
+        reboot = True
+    else:
+        print("fstab  already installed")
+    sys.stdout.flush()
+    exit(0)
+
+def clean_paths():
+    # replace static paths with install path
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' airiana-core.py")
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' public/ip-replace.sh")
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' public/controller.py")
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' forcast2.0.py")
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' humtest.py")
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' updater.py")
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' systemfiles/controller.service")
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' systemfiles/airiana.service")
+    os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' public/ip-util.sh")
+
+
+user_id = pwd.getpwuid()
 user_name = os.getlogin()
 group_id = os.getgid()
 
@@ -24,110 +104,71 @@ print("Installing the AirianaCores")
 reboot = False
 os.system("apt-get update")
 path = os.getcwd()
-if "clean" in sys.argv:
-    os.system("systemctl disable controller.service")
-    os.system("systemctl disable airiana.service")
-    os.system("rm /etc/systemd/airiana.service /etc/systemd/system/controller.service")
 
-# INSTALL DEPS#
-os.system("apt-get -y install python3-pip")
-os.system("apt-get -y install python3-dev")
-os.system("apt-get -y install python3-matplotlib")
-os.system("pip3 install minimalmodbus")
-os.system("pip3 install progressbar")
-os.system("pip3 install requests")
-os.system("pip3 install pyModbusTCP")
-os.system("pip3 install setuptools")
-os.system("pip3 install pyephem")
-os.system("pip3 install numpy")
-os.system("apt-get -y install ntp")
-os.system("apt-get -yq --force-yes -o \"Dpkg::Options::=--force-confdef\"  upgrade")
+if "clean" in sys.argv:
+    os.system("sudo systemctl disable controller.service")
+    os.system("sudo systemctl disable airiana.service")
+    os.system("sudo rm /etc/systemd/airiana.service /etc/systemd/system/controller.service")
+    exit(0)
+
+if len(sys.argv) == 1:
+    install_deps()
+
 # NEED TO SET LOCALE
 # os.system("cp ./systemfiles/timezone /etc/")
 
-# Enable UART for RS485 HAT
-boot_cmd = "enable_uart=1\n"
-boot_file = open("/boot/config.txt", "r+")
-lines = boot_file.readlines()
-if boot_cmd in lines:
-    print("Uart already enabled")
+if user_id == 0 and --set-uart in sys.argv:
+    setUart()
 else:
-    print("Enabling uart")
-    boot_file.write(boot_cmd)
-    boot_file.close()
-    reboot = True
-sys.stdout.flush()
+    os.popen("sudo ./install.py --set-uart")
+# Fix static paths
+clean_paths()
 
-# replace static paths with install path
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' airiana-core.py")
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' public/ip-replace.sh")
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' public/controller.py")
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' forcast2.0.py")
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' humtest.py")
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' updater.py")
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' systemfiles/controller.service")
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' systemfiles/airiana.service")
-os.system("sed -i 's-/home/pi/airiana/-" + path + "/-g' public/ip-util.sh")
-
-# MAKE RAM DRIVE IN FSTAB#
-fstab_comment = "#temp filesystem only in RAM for use on Airiana tempfiles.\n"
-fstab_RAM = "tmpfs " + path + "/RAM tmpfs defaults,noatime,nosuid,mode=0755,size=50m 0 0\n"
-# MAKE RAM DRIve for linux logs var/logs
-fstab_var = "tmpfs /var/log tmpfs defaults,noatime,nosuid,mode=0755,size=75m 0 0"
-fstab_file = open("/etc/fstab", "r+")
-lines = fstab_file.readlines()
-
-# Write RAM tmpfs's to fstab
-if fstab_var not in lines or fstab_RAM not in lines:
-    for each in lines:
-        if "/RAM" in each:
-            print("Found RAM drive setup")
-            lines.pop(lines.index(each))
-        if "/var" in each:
-            print("Found /var/log partition setup")
-            lines.pop(lines.index(each))
-        if "filesystem only in RAM" in each:
-            print("Found fstab comment")
-            lines.pop(lines.index(each))
-    print("Setting up Ram drive")
-    lines.append(fstab_comment)
-    lines.append(fstab_RAM)
-    lines.append(fstab_var)
-    fstab_file.seek(0, 0)
-    fstab_file.writelines(lines)
-    fstab_file.close()
-    reboot = True
+# update the fstab file with RAM-drives for /var/log and ~/airiana/RAM
+if user_id == 0 and --set-fstab in sys.argv():
+    user_id = sys.argv[-2]
+    group_id = sys.argv[-1]
+    setFstab()
 else:
-    print("fstab  already installed")
-sys.stdout.flush()
+    os.popen("sudo ./install.py --set-fstab " + user_id + group_id)
 
 # install system services for airaina and controller
 if not os.path.lexists("/etc/systemd/system/airiana.service"):
     print("setup autostart for airiana.service")
-    os.system("cp ./systemfiles/airiana.service /etc/systemd/system/")
-    os.system("systemctl enable airiana.service")
+    os.system("sudo cp ./systemfiles/airiana.service /etc/systemd/system/")
+    os.system("sudo systemctl enable airiana.service")
 
 if not os.path.lexists("/etc/systemd/system/controller.service"):
     print("setup autostart for controller.service")
-    os.system("cp ./systemfiles/controller.service /etc/systemd/system/")
-    os.system("systemctl enable controller.service")
+    os.system("sudo cp ./systemfiles/controller.service /etc/systemd/system/")
+    os.system("sudo systemctl enable controller.service")
 sys.stdout.flush()
 
-# redir console from uart
+
 boot_cmd = "dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait"
-if boot_cmd not in open("/boot/cmdline.txt").read():
+# redir console from uart
+def redirectConsole(boot_cmd):
     print("adding boot cmdline config")
     os.system("echo " + boot_cmd + "> /boot/cmdline.txt")
-    reboot = True
+
+
+if boot_cmd not in open("/boot/cmdline.txt").read():
+    if user_id == 0 and "--redirect-console" in sys.argv:
+        redirectConsole(boot_cmd)
+        reboot = True
 
 # setup airiana as host#
-print("Copy hosts file")
-os.system("cp ./systemfiles/hosts /etc/")
+print("Copy hosts file and set hostname")
+os.system("sudo cp ./systemfiles/hosts /etc/")
+os.system("echo airiana |sudo tee /etc/hostname")
+
 sys.stdout.flush()
 
 # Touch a data log file
 print("Touch data.log")
 os.system("touch data.log")
+
+# move to public folder for symlink setup
 os.chdir("./public/")
 sys.stdout.flush()
 
@@ -138,9 +179,6 @@ os.system("ln -s ../RAM/out out.txt")
 os.system("ln -s ../RAM/history.png history.png")
 os.system("ln -s ../RAM/air.out air.out")
 os.system("ln -s ../RAM/status.html status.html")
-os.system("echo airiana > /etc/hostname")
-os.system("chown pi:pi ../RAM/")
-os.system("chown pi:pi ../RAM/*")
 
 # setup updater.py for auto update
 print("setup auto update from airiana repo.")
