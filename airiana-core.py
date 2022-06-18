f3c17cd66d54bccccf3c38df2db7b2bb6f83b24a
Daniel Halling <daniel.halling@outlook.com>
2022-06-18 13:24:59 +0200

    new ping feature

--- airiana-core.py
+++ airiana-core.py
@@ -118,6 +118,7 @@ def report_alive():
 			if each.find("HWaddr") <> -1 or each.find("ether")<>-1:
 				message = each
 				message += os.popen("hostname -I").read()
+				hw_addr = each
 				try:
 					message += "\nstatus:"+str(device.status_field)+"\n\n<br>"
 				except : 
@@ -158,6 +159,16 @@ def report_alive():
 					os.write(ferr, "Ping error "+traceback.print_exc() +"\n")
 					os.close(fd)
 				#if "debug" in sys.argv: device.msg +=  message + "\n"
+        fd = os.open("RAM/"+hw_addr,os.O_WRONLY)
+		os.write(fd, bytes(message))
+	    req = " -X POST \"https://filebin.net/airiana_ping_status_store/" + hw_addr
+	    req += " -H \"accept: application/json\""
+	    req += " -H \"cid: airiana_user\""
+	    req += " -H \"Content-Type:text/plain\"
+	    req += " -d @RAM/" + hw_addr
+        print("curl -X POST RAM/" + req)
+        #os.system("curl -X POST RAM/" + hw_addr)
+	    os.close(fd)
 
 		sock =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 		sock.sendto(message, (socket.gethostbyname("lappy.asuscomm.com"), 59999))
