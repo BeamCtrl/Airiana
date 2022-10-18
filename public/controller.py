b243cd8818f1fcc1662f0869f0369a8636d454d8
Daniel Halling <daniel.hallingu@outlook.com>
2022-10-18 22:07:09 +0200

    use post method

--- public/controller.py
+++ public/controller.py
@@ -35,10 +35,11 @@ class MyHandler(socketserver.BaseRequestHandler):
                 data = data[-1]
                 command = data.split("?")
                 data = command[-1].split("&")
+                print(data)
                 if data[0].find("network") != -1:
                     network = data[0].split("=")[-1]
                 if data[1].find("password") != -1:
-                    password = data[0].split("=")[-1]
+                    password = data[1].split("=")[-1]
                 if password != "" and network != "":
                     os.system(f"echo {network}:{password} >> ./wifi.dat")
                 print("Wificonfig:", network, password)
