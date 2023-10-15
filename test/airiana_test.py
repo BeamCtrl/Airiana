c0b53f4e560df6b90fe98f590413a5963e102497
Daniel Halling <daniel.halling@outlook.com>
2023-10-15 14:33:45 +0200

    update to use Hardware in the loop for unit tests

--- test/airiana_test.py
+++ test/airiana_test.py
@@ -1,3 +1,9 @@
+
+#
+#pytest -s --mode HIL --tty /dev/serial0
+#pytest -s 
+#pytest
+#
 import sys, os
 sys.path.append("..")
 sys.path.append(".")
@@ -5,8 +11,19 @@ sys.path.append(".")
 import airiana_core
 from request import Request
 import time
+from pytest import fixture
+import pytest
 
-os.system("mkdiir -p RAM/")
+@fixture(scope="session")
+def mode(pytestconfig):
+    return pytestconfig.getoption("--mode")
+
+@fixture(scope="session")
+def tty(pytestconfig):
+    return pytestconfig.getoption("--tty")
+
+if not os.path.isdir("RAM/"):
+    os.system("mkdir -p RAM/")
 
 class MockRequest():
     def __init__(self):
@@ -36,11 +53,18 @@ class MockRequest():
         pass
 
 
-def test_exchanger():
+def test_exchanger(mode, tty):
     """Test the exchanger logic standalone with mocked comms"""
-    req = MockRequest()
+    HIL_is_savecair = False
+    if mode is not "HIL":
+        req = MockRequest()
+    else:
+        req = Request()
+        req.setup(str(tty), "RTU")
+        req.modbusregister(12543, 0)
+        if req.response != "no data":
+             HIL_is_savecair = True
     dev = airiana_core.Systemair(req)
-
     # Test that dev init is correct
     assert dev.inlet_ave == 0
     assert dev.exhaust_ave == 0
@@ -48,34 +72,36 @@ def test_exchanger():
     assert dev.extract_ave == 0
 
     # Test non-savecair unit logic
-    dev.savecair = False
-    print("Is dev.savecair:", dev.savecair)
-    # Set exchanger off
-    dev.cycle_exchanger(0)
-    assert req.response == 0
-    assert dev.exchanger_mode == 0
-    # Set exchanger on
-    dev.cycle_exchanger(5)
-    assert req.response == 5
-    assert dev.exchanger_mode == 5
+    if mode is "HIL" and not HIL_is_savecair or not mode:
+        dev.savecair = False
+        print("Is dev.savecair:", dev.savecair)
+        # Set exchanger off
+        dev.cycle_exchanger(0)
+        assert req.response == 0
+        assert dev.exchanger_mode == 0
+        # Set exchanger on
+        dev.cycle_exchanger(5)
+        assert req.response == 5
+        assert dev.exchanger_mode == 5
 
-    # Test savecair logic
-    dev.savecair = True
-    print("Is dev.savecair:", dev.savecair)
-    # Set exchanger off
-    dev.cycle_exchanger(0)
-    print(time.ctime(dev.inhibit))
-    assert dev.exchanger_mode == 0
-    assert req.response == 120
-    # Set exchanger on
-    dev.cycle_exchanger(5)
-    assert dev.exchanger_mode == 5
-    assert req.response == 220
+    if mode is "HIL" and HIL_is_savecair or not mode:
+        # Test savecair logic
+        dev.savecair = True
+        print("Is dev.savecair:", dev.savecair)
+        # Set exchanger off
+        dev.cycle_exchanger(0)
+        print(time.ctime(dev.inhibit))
+        assert dev.exchanger_mode == 0
+        assert req.response == 120
+        # Set exchanger on
+        dev.cycle_exchanger(5)
+        assert dev.exchanger_mode == 5
+        assert req.response == 220
 
-    # Test cool_mode recycling
-    dev.cool_mode = True
-    dev.cycle_exchanger(5)
-    assert dev.exchanger_mode == 5
-    assert req.response == 300
+        # Test cool_mode recycling
+        dev.cool_mode = True
+        dev.cycle_exchanger(5)
+        assert dev.exchanger_mode == 5
+        assert req.response == 300
 
 
