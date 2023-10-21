fc2bed044319aa9995d69d59bec80f1157761d26
Daniel Halling <daniel.halling@outlook.com>
2023-10-21 21:38:51 +0200

    add cd to ip-replace

--- public/ip-replace.sh
+++ public/ip-replace.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 HOST_IP_ADDRESS=$(hostname -I | cut -f1 -d' ')
+cd public
 sed -r "s/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b"/$HOST_IP_ADDRESS/ buttons.html.template > buttons.html.new
 rm -f buttons.html
 mv buttons.html.new buttons.html
