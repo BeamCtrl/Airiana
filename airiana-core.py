1af1c9439a024587fb2c39b73302c700643e4ab9
Daniel Halling <daniel.halling@outlook.com>
2023-11-13 11:54:23 +0100

    add previous exchanger state to log printout on cycle_exchanger

--- airiana-core.py
+++ airiana-core.py
@@ -1385,7 +1385,9 @@ class Systemair(object):
 
     # change exchanger mode target is "to", if to = None, flip 0 or 5
     def cycle_exchanger(self, to=None):
-        os.write(ferr, bytes("cycle exchanger to: " + str(to) + "\t" + str(time.ctime()) + "\n", encoding='utf8'))
+        os.write(ferr, bytes("cycle exchanger to: " + str(to)
+                 + " from " + str(self.exchanger_mode) + "\t" 
+                 + str(time.ctime()) + "\n", encoding='utf8'))
         if not self.savecair:
             def set_val(val):
                 try:
